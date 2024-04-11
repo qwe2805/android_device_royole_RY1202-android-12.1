49598f159395069294bbd4c0a85b98e9c183bc23
qwe2805 <106086144+qwe2805@users.noreply.github.com>
2024-04-11 17:43:08 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_RY1202-user
-add_lunch_combo omni_RY1202-userdebug
-add_lunch_combo omni_RY1202-eng
+add_lunch_combo twrp_RY1202-user
+add_lunch_combo twrp_RY1202-userdebug
+add_lunch_combo twrp_RY1202-eng
